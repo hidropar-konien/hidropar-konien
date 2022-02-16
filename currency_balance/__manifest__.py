1f1de95ff87ec6b16e245bbb85574e41d859eed2
hidropar-konien <devops@konien.com>
2022-02-16 11:23:38 +0300

    [ADD] Aged Partner Currency Balance addon

---
+++ currency_balance/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Aged Account Currency Balance',
+    'version': '1.1',
+    'summary': 'Aged Account Currency Balance',
+    'description': """
+    """,
+    'category': 'Accounting',
+    'website': 'https://www.konien.com',
+    'images': [],
+    'depends': ['account'],
+    'data': [
+        'views/report_agedpartnercurrencybalance.xml',
+        'wizard/currency_balance_view.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ currency_balance/wizard/account_common_report.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Konien Ltd.Şti.
+
+from odoo import api, fields, models, _
+
+
+class AccountCommonReport(models.TransientModel):
+    _inherit = "account.common.report"
+
+    currency_id = fields.Many2one('res.currency')
+
+    def _build_contexts(self, data):
+        result = {}
+        result['journal_ids'] = 'journal_ids' in data['form'] and data['form']['journal_ids'] or False
+        result['state'] = 'target_move' in data['form'] and data['form']['target_move'] or ''
+        result['date_from'] = data['form']['date_from'] or False
+        result['date_to'] = data['form']['date_to'] or False
+        result['strict_range'] = True if result['date_from'] else False
+        result['currency_id'] = data['form']['currency_id'] or False
+        return result
+
+    @api.multi
+    def check_report(self):
+        self.ensure_one()
+        data = {}
+        data['ids'] = self.env.context.get('active_ids', [])
+        data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
+        data['form'] = self.read(['date_from', 'date_to', 'journal_ids', 'target_move', 'currency_id'])[0]
+        used_context = self._build_contexts(data)
+        data['form']['used_context'] = dict(used_context, lang=self.env.context.get('lang') or 'en_US')
+        return self._print_report(data)
+
+

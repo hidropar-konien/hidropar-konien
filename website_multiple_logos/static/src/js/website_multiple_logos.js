da026f60ef49c705dbc9b942da2d7cdc8e292a29
konien-makiraz <mehmetalikiraz@konien.com>
2025-08-21 12:18:16 +0300

    header alanı sticky uygulandı.

--- website_multiple_logos/static/src/js/website_multiple_logos.js
+++ website_multiple_logos/static/src/js/website_multiple_logos.js
@@ -4,14 +4,13 @@ odoo.define('website_multiple_logos.sticky_header', function (require) {
 var publicWidget = require('web.public.widget');
 
 publicWidget.registry.stickyHeader = publicWidget.Widget.extend({
-    selector: 'header#top', // Odoo'nun ana header elementini hedefler
+    selector: 'header#top',
     disabled: false,
 
     /**
      * @override
      */
     start: function () {
-        // Website editör modunda bu özelliği devre dışı bırak
         if (this.editableMode) {
             this.disabled = true;
             return this._super.apply(this, arguments);
@@ -20,12 +19,10 @@ publicWidget.registry.stickyHeader = publicWidget.Widget.extend({
         this.header = this.$el;
         this.headerHeight = this.header.outerHeight();
         this.stickyPoint = this.header.offset().top;
-
-        // Performans için scroll olayını throttle ile yavaşlatıyoruz
         this.scrollFunction = _.throttle(this._updateHeader.bind(this), 100);
         $(window).on('scroll.sticky_header', this.scrollFunction);
 
-        this._updateHeader(); // Sayfa ilk yüklendiğinde kontrol et
+        this._updateHeader();
 
         return this._super.apply(this, arguments);
     },
